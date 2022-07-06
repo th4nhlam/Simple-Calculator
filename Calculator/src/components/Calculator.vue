57b4fd520b4c11b79962ca557770f0fdeaf86d2f
Lam Tran Thanh Lam <lam73nk@gmail.com>
2022-07-06 13:47:23 +0700

    First commit, still have error on position

---
+++ Calculator/src/components/Calculator.vue
@@ -0,0 +1,194 @@
+<template>
+<div id="may_tinh">
+   <div id="ket_qua">
+       <div id="ket_qua_cu">
+         <p id="gia_tri_cu"></p>
+      </div>
+      <div id="xuat_ket_qua">
+         <p id="gia_tri_xuat"></p>
+      </div>
+   </div>
+   <div id="ban_phim"></div>
+   <div id="ban_phim"> 
+     <button class="he_thong" id="xoa_tat_ca">C</button>
+     <button class="he_thong" id="xoa_tung_so">CE</button>
+     <button class="he_thong" id="%">%</button>
+     <button class="he_thong" id="/">÷</button>
+     <button class="con_so" id="7">7</button>
+     <button class="con_so" id="8">8</button>
+     <button class="con_so" id="9">9</button>
+     <button class="he_thong" id="*">×</button>
+     <button class="con_so" id="4">4</button>
+     <button class="con_so" id="5">5</button>
+     <button class="con_so" id="6">6</button>
+     <button class="he_thong" id="-">-</button>
+     <button class="con_so" id="1">1</button>
+     <button class="con_so" id="2">2</button>
+     <button class="con_so" id="3">3</button>
+     <button class="he_thong" id="+">+</button>
+     <button class="trong" id="trong"></button>
+     <button class="con_so" id="0">0</button>
+     <button class="trong" id="trong"></button>
+     <button class="he_thong" id="=">=</button>
+ </div>
+</div>
+      
+</template>
+
+<script>
+export default {
+  name: 'Calc-ulator',
+  data(){function Lay_gia_tri_cu(){
+     return document.getElementById("gia_tri_cu").innerText;
+ }
+function In_gia_tri_cu(so) {
+     document.getElementById("gia_tri_cu").innerText = so;
+ }
+function Lay_ket_qua() {
+     return document.getElementById("gia_tri_xuat").innerText;
+ }
+
+ function Dinh_dang_chuoi(so) {
+      var n = Number(so);
+      var gia_tri = n.toLocaleString("en");
+      return gia_tri;
+}
+function Xoa_Dinh_dang_chuoi(so) {
+     return Number(so.replace(/,/g, ''))
+ }
+ var con_so = document.getElementsByClassName('con_so');
+ for(var i=0; i<con_so.length; i++) {
+     con_so[i].addEventListener('click', function() {
+         var ket_qua = Xoa_Dinh_dang_chuoi(Lay_ket_qua())
+         if(ket_qua != isNaN()) {
+             ket_qua = ket_qua + this.id;
+             In_Ket_qua(ket_qua)
+         }
+     })
+ }
+ function In_Ket_qua(so) {
+     if(so=="") {
+         document.getElementById("gia_tri_xuat").innerText = so;
+     } else {
+         document.getElementById("gia_tri_xuat").innerText = Dinh_dang_chuoi(so);
+     }
+ }
+
+var he_thong = document.getElementsByClassName('he_thong'); 
+ for(let i=0; i < he_thong.length; i++) { 
+     he_thong[i].addEventListener('click', function() { 
+         if(this.id == "xoa_tat_ca") { 
+             In_Ket_qua(""); 
+             In_gia_tri_cu(""); 
+         } 
+         else if(this.id == "xoa_tung_so") { 
+             let ket_qua = Xoa_Dinh_dang_chuoi(Lay_ket_qua()).toString(); 
+             if(ket_qua) { 
+                 ket_qua = ket_qua.substr(0, ket_qua.length -1) 
+                 In_Ket_qua(ket_qua) 
+             } 
+         } 
+         else { 
+             var ket_qua = Lay_ket_qua(); 
+             var ket_qua_cu = Lay_gia_tri_cu(); 
+             if(ket_qua != "" ) { 
+                 ket_qua = Xoa_Dinh_dang_chuoi(ket_qua); 
+                 ket_qua_cu =ket_qua_cu + ket_qua; 
+                 if(this.id == "=") { 
+                     var ket_qua_cuoi = eval(ket_qua_cu); 
+                     In_Ket_qua(ket_qua_cuoi) 
+                     In_gia_tri_cu("") 
+                 } else { 
+                     ket_qua_cu = ket_qua_cu + this.id; 
+                     In_gia_tri_cu(ket_qua_cu) 
+                     In_Ket_qua("") 
+                 } 
+             } 
+         } 
+     }) 
+ } 
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+body{
+     font-family: Roboto;
+     background: #373B44;
+     background: -webkit-linear-gradient(to right, #4286f4, #373B44);
+     background: linear-gradient(to right, #4286f4, #373B44);
+ }
+#may_tinh{
+     width:320px;
+     height:520px;
+     background-color: #eaedef;
+     top:20px;
+     margin:0 auto;
+     position:relative;
+     border-radius: 5px;
+     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+ }
+#ket_qua{
+     height: 120px;
+ }
+#ket_qua_cu{
+     text-align: right;
+     height:20px;
+     margin:0 20px;
+     padding-top:20px;
+     font-size:15px;
+     color:#919191;
+ }
+#xuat_ket_qua {
+     text-align: right;
+     height:60px;
+     margin: 10px 20px;
+     font-size: 30px;
+ }
+#ban_phim{
+     height: 400px;
+ }
+ .he_thong, .con_so, .trong {
+     width:50px;
+     height:50px;
+     margin: 15px;
+     float:left;
+     border-radius: 50%;
+     border-width: 0;
+     font-weight:bold;
+     font-size:16px;
+ }
+.con_so, .trong{
+     background-color: #eaedef;
+ }
+.he_thong, .con_so{
+     cursor: pointer;
+ }
+.he_thong:active, .con_so:active {
+     font-size: 13px;
+ }
+.he_thong:focus, .con_so:focus, .trong:focus{
+     outline: 0;
+ }
+button:nth-child(4) {
+     font-size: 20px;
+     background-color: #20b2aa;
+ }
+button:nth-child(8) {
+     font-size: 20px;
+     background-color: #ffa500;
+ }
+button:nth-child(12) {
+     font-size: 20px;
+     background-color: #f08080;
+ }
+button:nth-child(16) {
+     font-size: 20px;
+     background-color: #7d93e0;
+ }
+button:nth-child(20) {
+     font-size: 20px;
+     background-color: #9477af;
+ }
+</style>
